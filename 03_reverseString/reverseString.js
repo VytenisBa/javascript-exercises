bda0ca6d27bcf8b650cd1ba76bf5cf24c67fe308
VytenisBa <vbgithub@gmail.com>
2022-08-31 05:57:21 +0300

    3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    return s.split('').reverse().join('');
 };
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
