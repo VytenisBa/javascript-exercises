8c25a31bf061fc14ee23298f79173c3a664ddda0
VytenisBa <vbgithub@gmail.com>
2022-09-03 10:22:05 +0300

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel= ((fah - 32) * 5/9).toFixed(1);
+  let celNum = parseFloat(cel);
+  return celNum;
 };
 
-const ctof = function() {
 
+const ctof = function(cel) {
+  let fah= (cel * 9/5 + 32).toFixed(1);
+  let fahNum = parseFloat(fah);
+  return fahNum;
 };
+
+console.log(ctof(-10))
+
 
 // Do not edit below this line
 module.exports = {
