a1c3a85312a03fb2c2fca8bba221067c569fa1ba
VytenisBa <vbgithub@gmail.com>
2022-09-03 08:48:45 +0300

    6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(a) {
+    if (a % 4 === 0) {
+        if (a % 100 === 0){
+            if (a % 400 == 0){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else{
+        return false;
+    }
 };
+
+console.log(leapYears(1996));
+
 
 // Do not edit below this line
 module.exports = leapYears;
