a66efdaac919adc6c2bddd53fb3405c024aaf1ae
VytenisBa <vbgithub@gmail.com>
2022-09-03 10:26:50 +0300

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,10 @@
 const repeatString = function(a, b) {
+    if(b < 0){
+        return 'ERROR';
+    }
     return a.repeat(b);
 };
-console.log(repeatString('hey', 3));
+console.log(repeatString('hey', -1));
 
 
 // Do not edit below this line
